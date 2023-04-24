24d33a589679ddd20ff66fffd50082ce6ef7b85c
ejk9658 <ejk9658@naver.com>
2023-04-24 17:48:23 +0900

    Add : OAuth2.0 네이버 로그인 추가

--- springboot_security/security1/src/main/java/com/cos/security1/config/auth/PrincipalDetails.java
+++ springboot_security/security1/src/main/java/com/cos/security1/config/auth/PrincipalDetails.java
@@ -22,15 +22,25 @@ import java.util.Map;
 일반 로그인 -> UserDetails
 SNS 로그인 -> OAuth2User
 PrincipalDetails 타입으로 이 둘을 묶어서 이것을 찾도록 함.
+
+즉, PrincipalDetails 타입을 이용하면 된다!
  */
 
 @Data
 public class PrincipalDetails implements UserDetails, OAuth2User {
 
     private User user;  // 컴포지션
+    private Map<String, Object> attributes;
 
+    // 일반 로그인
     public PrincipalDetails(User user) {
         this.user = user;
+    }
+
+    // OAuth 로그인
+    public PrincipalDetails(User user, Map<String, Object> attributes) {
+        this.user = user;
+        this.attributes = attributes;
     }
 
     // 해당 User의 권한을 리턴하는 곳
@@ -82,7 +92,7 @@ public class PrincipalDetails implements UserDetails, OAuth2User {
 
     @Override
     public Map<String, Object> getAttributes() {
-        return null;
+        return attributes;
     }
 
     @Override
